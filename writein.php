5123d31216abd20e9358a358856fc2bb5b8b1fb2
Kocsis Krisztian <pulsarxp@gmail.com>
2025-01-01 20:42:37 +0100

    AddWriteInPhpFile

---
+++ writein.php
@@ -0,0 +1,43 @@
+<table width="600" border="0" align="center" cellpadding="3" cellspacing="0"> <tr>
+	<td><strong>New Line</strong></td>
+</tr>
+
+</table>
+	<table width="600" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<form id="form" name="form" method="post" action="add.php">
+<td>
+<table width="800" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td></td>
+<td></td>
+<td>Name</td>
+<td>URL</td>
+<td>Comment:</td>
+<td>Camera type</td>
+<td>Category</td>
+</tr>
+<tr>
+<td valign="top">Napi</td>
+<td valign="top">:</td>
+	<td><textarea name="name" cols="20" rows="1" id="name"></textarea></td>
+	<td><textarea name="url" cols="20" rows="1" id="url"></textarea></td>
+	<td><textarea name="comment" cols="30" rows="1" id="comment"></textarea></td>
+	<td><textarea name="cam_type" cols="5" rows="1" id="cam_type"></textarea></td>
+	<td><textarea name="category" cols="5" rows="1" id="category"></textarea></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+	<td><input type="submit" name="Küld" value="Küld" /> <input type="reset" name="Submit2" value="Reset" /></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
+	<table width="400" border="0" align="center" cellpadding="3" cellspacing="0">
+<tr>
+<td><strong><a href="pan.php">Megtekintés</a> </strong></td>
+</tr>
+</table>
